ba66b83adc7952708c62a4f5402075f997c2012f
hsh <hsh@ebidding.net.cn>
2019-08-08 20:33:55 +0800

    配置路由访问的demo

---
+++ talkshop-item-service/src/main/java/cn/com/talkshop/Item/service/ItemService.java
@@ -0,0 +1,14 @@
+package cn.com.talkshop.Item.service;
+
+import cn.com.talkshop.Item.model.Item;
+
+import java.util.List;
+
+/**
+ * @Author: hsh
+ * @Date: 2019/8/8 8:17 PM
+ * @Description:
+ */
+public interface ItemService {
+    public List<Item> getItems();
+}
