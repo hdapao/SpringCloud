06299d88e402d2a882341db67d6e2dc98bd4901b
hsh <hsh@ebidding.net.cn>
2020-12-21 13:04:46 +0800

    增加feign ribbon的依赖

---
+++ common/src/main/java/cn/com/hdapao/api/IDemoApiService.java
@@ -0,0 +1,9 @@
+package cn.com.hdapao.api;
+
+/**
+ * @Author: hsh
+ * @Date: 2020/12/21 12:35 下午
+ * @Description:
+ */
+public interface IDemoApiService {
+}
---
+++ eureka-client-two/src/main/java/cn/com/hdapao/demo/api/DemoApiServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.com.hdapao.demo.api;
+
+import cn.com.hdapao.api.IDemoApiService;
+
+/**
+ * @Author: hsh
+ * @Date: 2020/12/21 12:40 下午
+ * @Description:
+ */
+public class DemoApiServiceImpl implements IDemoApiService {
+
+}
